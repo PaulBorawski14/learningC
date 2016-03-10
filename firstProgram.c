a94c0f9b7c1e8599b6e837d8cb747e93dfcf7101
Paul Borawski <paulborawski14@gmail.com>
2016-03-10 14:02:16 -0800

    first commit

---
+++ firstProgram.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* MAKES AN INT. */
+int main(int argc, char *argv[])
+{
+    //set int distance to 100
+    int distance = 100;
+
+    // prints
+    printf("You are %d miles away.\n", distance);
+
+    return 0;
+}
---
+++ readme.markdown
@@ -0,0 +1,2 @@
+#C programming
+ this is where I will be posting my practice programs in C
